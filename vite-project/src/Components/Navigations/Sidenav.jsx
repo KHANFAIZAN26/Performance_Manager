2abc9a87769e177298efc24b080e4b98b3ac6fa9
Khan Faizan <97592600+KHANFAIZAN26@users.noreply.github.com>
2024-06-05 11:22:37 +0000

    Updation

--- vite-project/src/Components/Navigations/Sidenav.jsx
+++ vite-project/src/Components/Navigations/Sidenav.jsx
@@ -19,10 +19,77 @@ import ListItemText from '@mui/material/ListItemText';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import {useNavigate} from 'react-router-dom'
 
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
 
 
-const Sidenav = () =>  {
-    const navigate = useNavigate()
+const Sidenav = () => {
+    
+   
     const theme = useTheme();
     const [open, setOpen] = useState(false);
 
@@ -33,6 +100,8 @@ const Sidenav = () =>  {
     const handleDrawerClose = () => {
         setOpen(false);
     };
+
+    const navigate = useNavigate()
 
     return (
         <Box sx={{ display: 'flex' }}>
@@ -65,7 +134,7 @@ const Sidenav = () =>  {
                 <Divider />
                 <List>
                     <ListItem disablePadding sx={{display:'block'}}>
-                        <ListItemButton  sx={{minHeight: 48,justifyContent: open ? 'initial' : 'center',px: 2.5}} onClick={() => navigate('/home')}>
+                        <ListItemButton  sx={{minHeight: 48,justifyContent: open ? 'initial' : 'center',px: 2.5}} onClick={() => {navigate('/home')}}>
                             <ListItemIcon sx={{minWidth: 0,mr: open ? 3 : 'auto',justifyContent: 'center',}}><HomeOutlinedIcon /></ListItemIcon>
                             <ListItemText sx={{ opacity: open ? 1 : 0 }}>Home</ListItemText>
                         </ListItemButton>
